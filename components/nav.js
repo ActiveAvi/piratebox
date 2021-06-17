088fb7e13e5e9394eaec241adff413fdd1cbb35d
ActiveAvi <loch.hollows@gmail.com>
2021-06-17 00:37:46 -0400

    hover color

--- components/nav.js
+++ components/nav.js
@@ -8,7 +8,7 @@ const classes = {
     } z-20 transition duration-300 transform fixed msm:inset-x-0 sm:right-0 top-0 sm:pr-4 sm:mt-4 text-center `,
   innerNav: 'p-4 bg-opacity-90 bg-background sm:shadow-sm',
   button: 'sm:hidden bg-opacity-60 bg-background p-1 rounded-md m-1',
-  item: 'py-2',
+  item: 'py-2 hover:text-h-1',
   image: 'sm:w-32 opacity-60',
   brand: '',
   brandBox: 'bg-opacity-60 bg-background text-xl font-mono text-h-1 font-bold',
