a87c94979be6b4a282c1f8472b7b6b02bfbadfd4
ActiveAvi <loch.hollows@gmail.com>
2021-06-18 13:13:43 -0400

    added next-config, added assetprefix urls > relatv

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 const optimizedImages = require('next-optimized-images')
 const withPlugins = require('next-compose-plugins')
 
-module.exports = withPlugins([[optimizedImages]])
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.node = {
+        fs: 'empty',
+      }
+    }
+    return config
+  },
+  pageExtensions: ['js', 'jsx'],
+  target: 'serverless',
+  trailingSlash: true,
+  assetPrefix: './',
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' },
+    }
+  },
+}
+
+module.exports = withPlugins([[optimizedImages]], nextConfig)
